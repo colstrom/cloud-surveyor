c1cd8718e08b41a66b3be22adc39847c59dad21c
Chris Olstrom <chris@olstrom.com>
2016-08-08 14:00:50 -0700

    Add worker to survey tags

---
+++ lib/workers/tag_surveyor.rb
@@ -0,0 +1,15 @@
+require 'shoryuken'
+require_relative 'tag_cacher'
+require_relative 'support/ec2'
+
+class TagSurveyor
+  include ::Shoryuken::Worker
+
+  shoryuken_options queue: 'ec2-stale-tags', auto_delete: true
+
+  def perform(_, region)
+    ::Support::EC2.new(region).tags.value.each do |tag|
+      TagCacher.perform_async(tag)
+    end
+  end
+end
