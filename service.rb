8d0fecf4c19192acc90f91e155b7e106c3ee38ce
Chris Olstrom <chris@olstrom.com>
2016-08-08 14:09:35 -0700

    Add service wrapper

---
+++ service.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+require_relative 'lib/workers/associates_availability_zones'
+require_relative 'lib/workers/instance_cacher'
+require_relative 'lib/workers/region_surveyor'
+require_relative 'lib/workers/tag_cacher'
+require_relative 'lib/workers/tag_surveyor'
