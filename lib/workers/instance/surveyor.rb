118ee1bbd7e4bac2e47a508d40166036f0808aaa
Chris Olstrom <chris@olstrom.com>
2016-08-09 09:58:45 -0700

    Rename RegionSurveyor to InstanceSurveyor

--- lib/workers/instance/surveyor.rb
+++ lib/workers/instance/surveyor.rb
@@ -1,15 +1,15 @@
 require 'shoryuken'
-require_relative 'instance_cacher'
-require_relative 'support/ec2'
+require_relative 'miner'
+require_relative '../support/ec2'
 
-class RegionSurveyor
+class InstanceSurveyor
   include ::Shoryuken::Worker
 
   shoryuken_options queue: 'ec2-stale-regions', auto_delete: true
 
   def perform(_, region)
     ::Support::EC2.new(region).instances.value.each do |instance|
-      InstanceCacher.perform_async(instance)
+      InstanceMiner.perform_async(instance)
     end
   end
 end
