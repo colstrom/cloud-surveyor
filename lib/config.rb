96bada6dfaf7af92cdae44e83cdd30def7e90802
Chris Olstrom <chris@olstrom.com>
2016-08-10 12:57:18 -0700

    Move config into library

---
+++ lib/config.rb
@@ -0,0 +1,21 @@
+require 'yaml'
+
+module CloudSurveyor
+  class Config
+    def path
+      @path ||= File.expand_path ENV.fetch('CONFIG_PATH') { './queues.yaml' }
+    end
+
+    def config
+      @config ||= File.exist?(path) ? YAML.load_file(path) : {}
+    end
+
+    def endpoint
+      @endpoint ||= config.dig('aws', 'sqs_endpoint')
+    end
+
+    def queues
+      @queues ||= config.fetch('queues') { [] }.map(&:first)
+    end
+  end
+end
